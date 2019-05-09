4492dbf2e28ad8ace72c115cefb5896a15173cc5
VuTien <tienle.clc11t@gmail.com>
2019-05-09 11:33:03 +0700

    Start

---
+++ TheBigNerdRanchGuide/TheBigNerdRanchGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheBigNerdRanchGuide
+//
+//  Created by iMacbook on 5/9/19.
+//  Copyright © 2019 IOTLink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
