5e6aef1547b41be03b3caf8bc888da4e185df2d2
VuTien <tienle.clc11t@gmail.com>
2019-05-09 14:42:03 +0700

    chapter1 - update

--- TheBigNerdRanchGuide/TheBigNerdRanchGuide/ViewController.swift
+++ TheBigNerdRanchGuide/TheBigNerdRanchGuide/ViewController.swift
@@ -13,46 +13,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capital Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
     var index = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        questLabel.text = questions[index]
     }
     
     @IBAction func onTapQuestion(_ sender: Any) {
-        switch index {
-        case 0:
-            questLabel.text = "What is 7 + 7?"
-            index += 1
-            break
-        case 1:
-            questLabel.text = "What is the capital Vermont?"
-            index += 1
-            break
-        case 2:
-            questLabel.text = "What is cognac made from?"
+        index += 1
+        if ( index >= questions.count )
+        {
             index = 0
-            break
-        default: break
         }
-        
+        questLabel.text = questions[index]
+        answerLabel.text = "???"
     }
     
     @IBAction func onTapAnswer(_ sender: Any) {
-        if let compare = questLabel.text {
-            switch compare {
-            case "What is 7 + 7?":
-                answerLabel.text = "14"
-                break
-            case "What is the capital Vermont?":
-                answerLabel.text = "Montpelier"
-                break
-            case "What is cognac made from?":
-                answerLabel.text = "Grapes"
-                break
-            default: break
-            }
-        }
+        answerLabel.text = answers[index]
     }
 }
